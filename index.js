557511a5675b2c47fb572ccc63448820ae562c4b
sunny-reducer-9560 <zahrasinan9650@gmail.com>
2020-01-23 23:29:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code here
+class Brekfast{
+  constructor(food,drink){
+  this.food=food;
+  this.drink=drink;
+}
+}
+class Lunch{
+  constructor(salad,soup,drink){
+    this.salad=salad;
+    this.soup=soup;
+    this.drink=drink;
+  }
+}
+class Dinner{
+  constructor(soup,entree,_dessert){
+    this.soup=soup;
+    this.salad=salad;
+    this._dessert=_dessert;
+  }
+}
