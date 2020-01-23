c9e7fc6a04eb606c256b3d94168f079dd9ff26f8
sunny-reducer-9560 <zahrasinan9650@gmail.com>
2020-01-23 23:49:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // Write your code here
-class Brekfast{
+class breakfast{
   constructor(food,drink){
   this.food=food;
   this.drink=drink;
 }
 }
+let breakfast=newBreakfast("hhh","hhhdd");
 class Lunch{
   constructor(salad,soup,drink){
     this.salad=salad;
@@ -12,10 +13,15 @@ class Lunch{
     this.drink=drink;
   }
 }
+let lanch=new Lanch("aa","lk","jhhg");
 class Dinner{
   constructor(soup,entree,_dessert){
     this.soup=soup;
-    this.salad=salad;
+    this.entree=entree;
     this._dessert=_dessert;
   }
 }
+let dinner=new Dinner("saa","dd","uy");
+breakfast;
+lanch;
+dinner;
